113c8fbab678836b08ecb7f26c79d306d545726f
Lourdesjupo <lourdes.jupo@gmail.com>
2023-05-31 21:07:22 +0200

    added js advanced functions for each of the processes separating the logic from what is shown in the markup and validations. Added visual improvements in css and gifs.

--- js/main.js
+++ js/main.js
@@ -1,60 +1,93 @@
 'use strict';
+const form = document.querySelector('.form');
 const btn = document.querySelector('.btn');
 const select = document.querySelector('.select');
 const amount = document.querySelector('.amount');
 const message = document.querySelector('.message');
 const remainingAmount = document.querySelector('.remainingAmount');
 const btnReset = document.querySelector('.btnReset');
+const img = document.querySelector('.img');
 
 let accEuros = 50;
 remainingAmount.textContent = `Saldo: ${accEuros}`;
 
+function printMessage (msg) {
+  message.innerHTML = msg
+}
+function printRemainingAmount (msg) {
+  remainingAmount.textContent = msg
+}
+
+function validateInputs() {
+  const betAmount = parseInt(amount.value)
+  if (isNaN(amount.value) || betAmount < 0) {
+    printMessage('Introduce un importe válido');
+    return false;
+  }
+  if ( betAmount > accEuros) {
+    printMessage('No tienes suficiente saldo para esta apuesta');
+    return false;
+  }
+  return true  
+}
+
 function reset() {
   accEuros = 50;
-  remainingAmount.textContent = `Saldo: ${accEuros}`;
-  message.innerHTML = '¡Vamos a jugar!';
+  printRemainingAmount( `Saldo: ${accEuros}`);
+  printMessage('¡Vamos a jugar!');
   btnReset.classList.add('noDisplay');
   btn.disabled = false;
-  select.value = '';
+  select.selectedIndex = 0;
   amount.value = '';
 }
 
-function feedbackPrint() {
+function feedbackPrint(resultBet) {
+  if (resultBet === true) {
+    printMessage ('Has ganado el doble de lo apostado 😀');
+    img.src = "./img/win.gif";
+  } else {
+    printMessage ('Has perdido lo apostado 😞');
+    img.src = './img/lose.gif';
+  }
+  printRemainingAmount (`Saldo: ${accEuros}`);
+}
+
+function calculateBet() {
   const selected = parseInt(select.value);
   const randomNumber = getRandomNumber(6);
   const tAmount = parseInt(amount.value);
-  if (typeof selected === 'number' && tAmount > 0) {
-    if (randomNumber === selected) {
-      accEuros += tAmount * 2;
-      message.innerHTML = 'Has ganado el doble de lo apostado 😀';
-      remainingAmount.textContent = `Saldo: ${accEuros}`;
-    } else {
-      accEuros = accEuros - tAmount;
-      message.innerHTML = 'Has perdido lo apostado 😞';
-      remainingAmount.textContent = `Saldo: ${accEuros}`;
-    }
+
+  if (randomNumber === selected) {
+    accEuros += tAmount * 2;
+    return true;
   } else {
-    message.innerHTML = 'Introduce un importe válido';
+    accEuros = accEuros - tAmount;
+    return false;
   }
 }
 
-function handleClick(ev) {
-  feedbackPrint();
+function handleSubmit(ev) {
+  ev.preventDefault();
+  if(!validateInputs()) {
+    return
+  }
+  const resultBet = calculateBet();
+  feedbackPrint(resultBet);
 
   if (accEuros >= 200) {
-    message.innerHTML = 'Ha ganado la Usuaria';
+    printMessage('Ha ganado la Usuaria');
     btnReset.classList.remove('noDisplay');
     btn.disabled = true;
   } else if (accEuros <= 0) {
-    message.innerHTML = 'Ha ganado la Computadora';
+    printMessage ('Ha ganado la Computadora');
     btnReset.classList.remove('noDisplay');
     btn.disabled = true;
   }
 }
-//Math.round(Math.random()*100)
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-btn.addEventListener('click', handleClick);
+form.addEventListener('submit', handleSubmit);
 btnReset.addEventListener('click', reset);
