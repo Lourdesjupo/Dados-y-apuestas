d3ec4658e5e0d72fc381f063fe50731bc36891fd
Lourdesjupo <lourdes.jupo@gmail.com>
2023-05-31 10:28:48 +0200

    add bonus

--- js/main.js
+++ js/main.js
@@ -3,18 +3,25 @@ const select = document.querySelector('.select');
 const amount = document.querySelector('.amount');
 const message = document.querySelector('.message');
 const remainingAmount = document.querySelector('.remainingAmount');
+const btnReset = document.querySelector('.btnReset');
 
+let accEuros = 50;
+remainingAmount.textContent = `Saldo: ${accEuros}`;
 
-let accEuros = 50
+function reset() {
+  accEuros = 50;
+  remainingAmount.textContent = `Saldo: ${accEuros}`;
+  message.innerHTML = '¡Vamos a jugar!';
+  btnReset.classList.add('noDisplay');
+  btn.disabled = false;
+}
 
 function handleClick(ev) {
   const selected = parseInt(select.value);
   const randomNumber = getRandomNumber(6);
   const tAmount = parseInt(amount.value);
-  console.log(selected)
-  console.log(tAmount)
 
-  if(typeof(selected) === "number" && tAmount > 0) {
+  if (typeof selected === 'number' && tAmount > 0) {
     if (randomNumber === selected) {
       accEuros += tAmount * 2;
       message.innerHTML = 'Has ganado el doble de lo apostado 😀';
@@ -24,15 +31,23 @@ function handleClick(ev) {
       message.innerHTML = 'Has perdido lo apostado 😞';
       remainingAmount.textContent = `Saldo: ${accEuros}`;
     }
-  }else {
-    message.innerHTML = 'Introduce un importe válido'
+  } else {
+    message.innerHTML = 'Introduce un importe válido';
   }
-
+  if (accEuros >= 200) {
+    message.innerHTML = 'Ha ganado la Usuaria';
+    btnReset.classList.remove('noDisplay');
+    btn.disabled = true
+  } else if (accEuros <= 0) {
+    message.innerHTML = 'Ha ganado la Computadora';
+    btnReset.classList.remove('noDisplay');
+    btn.disabled = true;
+  }
 }
 //Math.round(Math.random()*100)
 function getRandomNumber(max) {
-  return Math.ceil(Math.random()*max);
+  return Math.ceil(Math.random() * max);
 }
 
-
 btn.addEventListener('click', handleClick);
+btnReset.addEventListener('click', reset);
