107a998bb46e9286009fd80978b4ced6019f824b
Lourdesjupo <lourdes.jupo@gmail.com>
2023-05-31 10:06:51 +0200

    add styles

--- js/main.js
+++ js/main.js
@@ -10,17 +10,22 @@ let accEuros = 50
 function handleClick(ev) {
   const selected = parseInt(select.value);
   const randomNumber = getRandomNumber(6);
-  const tAmount = amount.value;
+  const tAmount = parseInt(amount.value);
   console.log(selected)
-  console.log(randomNumber)
-  if(randomNumber === selected) {
-    accEuros+=(tAmount * 2)
-    message.innerHTML = 'Has ganado el doble de lo apostado 😀';
-    remainingAmount.textContent = `Saldo: ${accEuros}`
-  } else {
-    accEuros = accEuros - tAmount;
-    message.innerHTML = 'Has perdido lo apostado 😞';
-    remainingAmount.textContent = `Saldo: ${accEuros}`;
+  console.log(tAmount)
+
+  if(typeof(selected) === "number" && tAmount > 0) {
+    if (randomNumber === selected) {
+      accEuros += tAmount * 2;
+      message.innerHTML = 'Has ganado el doble de lo apostado 😀';
+      remainingAmount.textContent = `Saldo: ${accEuros}`;
+    } else {
+      accEuros = accEuros - tAmount;
+      message.innerHTML = 'Has perdido lo apostado 😞';
+      remainingAmount.textContent = `Saldo: ${accEuros}`;
+    }
+  }else {
+    message.innerHTML = 'Introduce un importe válido'
   }
 
 }
